b9b4c80cb3b8808a1953a58e4a3e1e210220a2a5
Akin Alkan <akinalkan45@gmail.com>
2023-03-15 02:13:55 +0300

    1

---
+++ src/test/java/Tests/C28_Put_DeSerialization.java
@@ -0,0 +1,24 @@
+package Tests;
+
+public class C28_Put_DeSerialization {
+    /*
+       https://jsonplaceholder.typicode.com/posts/70 url'ine asagidaki body’e sahip
+       bir PUT request yolladigimizda donen response’in response body’sinin asagida verilen ile ayni
+       oldugunu test ediniz
+       DE-SERIALIZATION ILE EXPECTED DATA TESTI
+       Expected Data :
+       {
+       "title": "Ahmet",
+       "body": "Merhaba",
+       "userId": 10,
+       "id": 70
+       }
+       Request Body
+       {
+       "title": "Ahmet",
+       "body": "Merhaba",
+       "userId": 10,
+       "id": 70
+       }
+     */
+}
